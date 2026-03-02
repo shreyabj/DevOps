fb35afd094b9cb6d352a9e87cae69479a21afd57
Shreya BJ <shreyabj18@gmail.com>
2026-03-02 10:05:30 +0530

    Fix formatting and indentation in HealthCheck.java

--- HealthCheck.java
+++ HealthCheck.java
@@ -1,12 +1,12 @@
 public class HealthCheck {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception 
 
         Process p = Runtime.getRuntime()
                 .exec(new String[]{"bash", "-c", "systemctl is-active --quiet nginx"});
 
         if (p.waitFor() == 0)
-            System.out.println("Nginx is running ✅");
+            System.out.println("Nginx is running ✅")
         else
-            System.out.println("Nginx is down ❌");
+            System.out.println("Nginx is down ❌")
     
 
